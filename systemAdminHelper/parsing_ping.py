2f2e3cbc7a610eb0cae403981a30909430299c15
canadianFisherman <104010562+canadianFisherman@users.noreply.github.com>
2023-03-30 10:06:49 +0300

    Add files via upload

---
+++ systemAdminHelper/parsing_ping.py
@@ -0,0 +1,25 @@
+import os
+
+
+def parse(ip, packets, timeout):
+    request = list(os.popen(f'ping {ip} -n {packets} -w {timeout}'))
+
+    if len(request) >= 9:
+        value = f'Проверка подключения к {ip}\nПакетов отправлено: {request[-3].split()[3].rstrip(",")}\n' \
+                f'Пакетов получено: {request[-3].split()[6].rstrip(",")}\nПакетов потеряно: {request[-3].strip().split()[9]}' \
+                f'\n' \
+                f'Приблизительное время приёма-передачи в мс:\n' \
+                f'Минимальное = {request[-1].split()[2]} Максимальное = {request[-1].split()[5]} Среднее = ' \
+                f'{request[-1].split()[-1]}'
+    elif packets == "1":
+        value = f''
+    elif len(request) == 1:
+        value = f'Хоста с таким именем не существует!'
+    else:
+        value = f'Проверка подключения к {ip}\nПакетов отправлено: {request[-1].split()[3].rstrip(",")}\n' \
+                f'Пакетов получено: {request[-1].split()[6].rstrip(",")}\nПакетов потеряно: {request[-1].strip().split()[9]}'
+
+    return value
+
+
+print(parse('1.1.1.1', "1", "4"))
