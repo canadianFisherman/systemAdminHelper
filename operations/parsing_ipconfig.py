b2ccfbe31bade1dfdf4b1b8cc1fc6de4d450ed58
canadianFisherman <ivanxxflol@gmail.com>
2023-04-03 19:40:02 +0300

    Добавлены

---
+++ operations/parsing_ipconfig.py
@@ -0,0 +1,19 @@
+﻿import os
+request = list(os.popen('ipconfig'))
+devices = {}
+
+for i in range(len(request) - 1):
+    try:
+        if request[i][-2] == ':':
+            name = str(request[i]).rstrip(':\n')
+            details = []
+            j = i + 2
+
+            while j < len(request) and request[j] != '\n':
+                details.append(str(request[j]).rstrip('\n'))
+                j += 1
+            devices.update({name: details})
+
+    except IndexError:
+        pass
+
