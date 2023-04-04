b61742a55aa5243a992fdb4a5bc43735e8bf80b4
canadianFisherman <ivanxxflol@gmail.com>
2023-04-04 19:07:32 +0300

    Добавлены

--- operations/parsing_ping.py
+++ operations/parsing_ping.py
@@ -1,25 +1,43 @@
 ﻿import os
+import langdetect
 
 
 def parse_ping(ip, packets, timeout):
     request = list(os.popen(f'ping {ip} -n {packets} -w {timeout}'))
 
-    if len(request[-1].split()) == 9:
-        value = f'Проверка подключения к {ip}\nПакетов отправлено: {request[-3].split()[3].rstrip(",")}\n' \
-                f'Пакетов получено: {request[-3].split()[6].rstrip(",")}\nПакетов потеряно: {request[-3].strip().split()[9]}' \
-                f'\n' \
-                f'Приблизительное время приёма-передачи в мс:\n' \
-                f'Минимальное = {request[-1].split()[2]} Максимальное = {request[-1].split()[5]} Среднее = ' \
-                f'{request[-1].split()[-1]}'
+    if langdetect.detect(request[1]) == 'en':
 
-    elif len(request[-1].split()) == 12:
-        value = f'Проверка подключения к {ip}\nПакетов отправлено: {request[-1].split()[3].rstrip(",")}\n' \
-                f'Пакетов получено: {request[-1].split()[6].rstrip(",")}\nПакетов потеряно: {request[-1].strip().split()[9]}'
+        if len(request[-1].split()) == 9:
 
-    else:
-        value = f'Хоста с таким именем не существует!'
+            value = f'Проверка подключения к {ip}\nПакетов отправлено: {request[-3].split()[3].rstrip(",")}\n' \
+                    f'Пакетов получено: {request[-3].split()[6].rstrip(",")}\nПакетов потеряно: {request[-3].strip().split()[9]}' \
+                    f'\n' \
+                    f'Приблизительное время приёма-передачи в мс:\n' \
+                    f'Минимальное = {request[-1].split()[2]} Максимальное = {request[-1].split()[5]} Среднее = ' \
+                    f'{request[-1].split()[-1]}'
 
-    return value + '\n'
+        elif len(request[-1].split()) == 12:
+            value = f'Проверка подключения к {ip}\nПакетов отправлено: {request[-1].split()[3].rstrip(",")}\n' \
+                    f'Пакетов получено: {request[-1].split()[6].rstrip(",")}\nПакетов потеряно: {request[-1].strip().split()[9]}'
 
+        else:
+            value = f'Хоста с таким именем не существует!'
 
+        return value + '\n'
 
+    elif langdetect.detect(request[1]) == 'ru':
+        if len(request[-1].split()) == 11:
+            value = f'Проверка подключения к {ip}\nПакетов отправлено: {request[-5].split()[3].rstrip(",")}\n' \
+                    f'Пакетов получено: {request[-5].split()[6].rstrip(",")}\nПакетов потеряно: {request[-5].strip().split()[9]}' \
+                    f'\n' \
+                    f'Приблизительное время приёма-передачи в мс:\n' \
+                    f'Минимальное = {request[-1].split()[2]} Максимальное = {request[-1].split()[5]} Среднее = ' \
+                    f'{request[-1].split()[-1]}'
+
+        elif len(request[-1].split()) == 2:
+            value = f'Проверка подключения к {ip}\nПакетов отправлено: {request[-2].split()[3].rstrip(",")}\n' \
+                    f'Пакетов получено: {request[-2].split()[6].rstrip(",")}\nПакетов потеряно: {request[-2].strip().split()[9]}'
+        else:
+            value = f'Хоста с таким именем не существует!'
+
+        return value + '\n'
--- operations/parsing_tracert.py
+++ operations/parsing_tracert.py
@@ -4,15 +4,15 @@
 def parse_tracert(ip, jumps, timeout):
 
     request = list(os.popen(f'tracert -h {jumps} -w {timeout} {ip}'))
+    value = ''
     if len(request) == 1:
         value = 'Хост с таким именем не обнаружен!'
-    elif request[2] != '\n':
-        value = f'Определяем путь к {ip}\n'
-        for i in range(4, request.index('Trace complete.\n')):
-            value += request[i].lstrip(' ')
-        value += f'Трассировка успешно выполнена.'
     else:
-        for i in range(3, request.index('Trace complete.\n')):
+        value += f'Определяем путь к {ip}\n'
+        index_of_enter = request.index('\n', 1, len(request))
+        index_of_enter_next = request.index('\n', request.index('\n', 1, len(request)) + 1, len(request))
+        for i in range(index_of_enter + 1, index_of_enter_next):
             value += request[i].lstrip(' ')
         value += f'Трассировка успешно выполнена.'
+
     return value
